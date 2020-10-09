7591c02efa98f9d157c6025f5efece470a27ddc4
Dugald Steer <dugsteer@gmail.com>
2020-10-09 20:37:07 +0200

    Flashcard Template mode

--- script.js
+++ script.js
@@ -2,7 +2,23 @@
 const sidey = document.getElementById('sidebar');
 const loader = document.getElementById("loaderDiv");
 const extra = document.getElementById('info');
+const mainpic = document.getElementById('mainpic');
 const blue = window.matchMedia("(max-width: 800px)");
+
+
+//play sound
+
+function croaker(){
+    
+    mainpic.innerHTML = '<img src="img/froggy.png" alt="decorative">' 
+}
+
+function changefrog(){
+    const frogster = new Audio('sounds/frog.ogg')
+    frogster.play();
+    mainpic.innerHTML = '<img src="img/froggy2.png" alt="decorative">' 
+    setTimeout(croaker, 1000);
+}
 
 // Show and hide about info
 function show(){info.classList.toggle('xxc');};
@@ -22,77 +38,101 @@ function myFunction(x) {if (blue.matches) {sidey.classList.remove("block");}}
 function toggler() {sidey.classList.toggle("block");}
 myFunction(blue);
 
+//Change main pic
+
+// setInterval(picChanger, 5000);
+// picChanger();
+
+// function picChanger(){
+//   mainpic.innerHTML === '<img src="img/froggy.png" alt="decorative">' ? mainpic.innerHTML = '<img src="img/froggy2.png" alt="decorative">' :  mainpic.innerHTML ='<img src="img/froggy.png" alt="decorative">'
+// }
+
+//FLIP CARDS
+
+const inner = document.getElementById('inner');
+
+function flipMe(){
+    inner.classList.toggle('turnMe');
+}
+
 //CHANGE CARDS
-const count = document.getElementById("count");
-let imageArt = document.getElementById('image-art');
-let imageText = document.getElementById('image-text');
+const count = document.getElementById('count');
 
 var imageList = [
-  "aeroplane.jpg",
-  "bike.jpg",
-  "car.jpg",
-  "dog-sled.jpg",
-  "firetruck.jpg",
-  "gondola.jpg",
-  "helicopter.jpg",
-  "icebreaker.jpg",
-  "jetski.jpg",
-  "kayak.jpg",
-  "limousine.jpg",
-  "motorbike.jpg",
-  "narrowboat.jpg",
-  "offroad-vehicle.jpg",
-  "punt.jpg",
-  "quad-bike.jpg",
-  "rocket.jpg",
-  "sailboat.jpg",
-  "train.jpg",
-  "unicycle.jpg",
-  "van.jpg",
-  "wagon.jpg",
-  "X-wing.jpg",
-  "yacht.jpg",
-  "zepellin.jpg",
+    "bear.jpg",
+    "bird.jpg",
+    "butterfly.jpg",
+    "cat.jpg",
+    "chicken.jpg",
+    "cow.jpg",
+    "dog.jpg",
+    "dolphin.jpg",
+    "elephant.jpg",
+    "fish.jpg",
+    "frog.jpg",
+    "giraffe.jpg",
+    "goat.jpg",
+    "hare.jpg",
+    "hippo.jpg",
+    "horse.jpg",
+    "lion.jpg",
+    "monkey.jpg",
+    "mouse.jpg",
+    "parrot.jpg",
+    "penguin.jpg",
+    "pig.jpg",
+    "rabbit.jpg",
+    "seal.jpg",
+    "sheep.jpg",
+    "snake.jpg",
+    "squirrel.jpg",
+    "tiger.jpg",
+    "turtle.jpg"
 ];
 
-let y = imageText.innerHTML;
-let z = y.concat(".jpg");
+
+let y = flashcard__text.innerHTML;
+let z = y.concat('.png');
 let x = imageList.indexOf(z);
 
-
 function goForwards() {
-  if (x < imageList.length - 1) {
-    x++;
-    imageArt.src =  `/img/${imageList[x]}`;
-   imageText.textContent = `${imageList[x].slice(0, -4)}`;
-    count.innerHTML = `${x + 1}/${imageList.length}`;
-    return x;
-  } else {
-    imageArt.src = ` /img/${imageList[imageList.length - 1]}`;
-    imageText.textContent = `${imageList[imageList.length - 1].slice(0, -4)}`;
-    count.innerHTML = `${imageList.length}/${imageList.length}`;
-    x = 0;
-    x = imageList.length -1;
-    return x;
-  }
+
+    if (x < (imageList.length-1)) {
+        x++;
+            document.getElementById('art').src = ` img/${imageList[x]}`;
+    document.getElementById('flashcard__text').textContent = `${imageList[x].slice(0, -4)}`;
+    count.innerHTML = `${x+1}/${imageList.length}`;
+         return x;
+    } else {
+    
+        document.getElementById('art').src = ` img/${imageList[imageList.length-1]}`;
+        document.getElementById('flashcard__text').textContent = `${imageList[imageList.length-1].slice(0, -4)}`;
+        count.innerHTML = `${imageList.length}/${imageList.length}`;
+                 x = 0;
+        x = imageList.length;
+        return x;
+    }
 }
 
 function goBackwards() {
-  if (x > 0) {
-    x--;
-    imageArt.src =  `/img/${imageList[x]}`;
-    imageText.textContent = `${imageList[x].slice(0, -4)}`;
-    count.innerHTML = `${x + 1}/${imageList.length}`;
-    return x;
-  } else {
-    imageArt.src = ` /img/${imageList[0]}`;
-    imageText.textContent = `${imageList[0].slice(0, -4)}`;
+        if (x > 0) {
+            x--;
+                document.getElementById('art').src = ` img/${imageList[x]}`;
+    document.getElementById('flashcard__text').textContent = `${imageList[x].slice(0, -4)}`;
+    count.innerHTML = `${x+1}/${imageList.length}`;
+            return x;
+        } else {
+            document.getElementById('art').src = ` img/${imageList[0]}`;
+    document.getElementById('flashcard__text').textContent = `${imageList[0].slice(0, -4)}`;
     count.innerHTML = `${1}/${imageList.length}`;
-    x = 0;
-    return x;
-  }
-}
+             x = 0;
+             return x;
+        }
+    }
+    
+
 
 goForwards();
 goBackwards();
-
+  
+  
